721690868d5a33555d22c735b5f459bd25a4f8bf
Team Teledildonics <teledildonics@hmamail.com>
2015-08-27 02:23:44 +0200

    Negotiate set sizes, detect poor matches

--- poc.c
+++ poc.c
@@ -28,7 +28,7 @@ void sort_prefs(pref_t *the_set, unsigned int the_set_size){
     qsort(the_set, the_set_size, sizeof(pref_t), compare_prefs);
 }
 
-void init(unsigned char * the_filename, pref_t *the_set, unsigned char * the_ephemeral_key){
+unsigned int init(unsigned char * the_filename, pref_t *the_set, unsigned char * the_ephemeral_key){
     unsigned char pref[MAX_STRLEN];
     unsigned char hashed_pref[crypto_hash_sha256_BYTES];
     unsigned int i = 0;
@@ -44,28 +44,31 @@ void init(unsigned char * the_filename, pref_t *the_set, unsigned char * the_eph
         }
         fclose(prefs);
     }
-    for(; i < MAX_SET_SIZE; i++){
-        randombytes(the_set[i].encrypted_pref, POINT_SIZE);
-    }
+    return i;
 }
 
-void compare(pref_t *set_a, pref_t *set_b, unsigned char * data_file){
+unsigned int compare(pref_t *set_a, pref_t *set_b, unsigned char * data_file, int output, unsigned int active_set_size){
     int i;
     unsigned char pref[MAX_STRLEN];
     pref_t *found;
     FILE* prefs;
-    for(i = 0; i < MAX_SET_SIZE; i++){
-        found = bsearch(set_a+i, set_b, MAX_SET_SIZE, sizeof(pref_t), compare_prefs);
+    unsigned int matches = 0;
+    for(i = 0; i < active_set_size; i++){
+        found = bsearch(set_a+i, set_b, active_set_size, sizeof(pref_t), compare_prefs);
         if(found && set_a[i].offset != -1){
-            if(prefs = fopen(data_file, "r")){
-                fseek(prefs, set_a[i].offset, SEEK_SET);
-                if(fgets(pref, MAX_STRLEN, prefs)){
-                    printf("%s contains shared pref: %s", data_file, pref);
+            matches++;
+            if(output){
+                if(prefs = fopen(data_file, "r")){
+                    fseek(prefs, set_a[i].offset, SEEK_SET);
+                    if(fgets(pref, MAX_STRLEN, prefs)){
+                        printf("%s contains shared pref: %s", data_file, pref);
+                    }
+                    fclose(prefs);
                 }
-                fclose(prefs);
             }
         }
     }
+    return matches;
 }
 
 int network_ab[2], network_ba[2];
@@ -77,61 +80,92 @@ void* alice_thread(void* v){
     memset(alices_private_set, -1, sizeof(alices_private_set));
     unsigned char alices_ephemeral_key[POINT_SIZE];
 
+    // initialize key
     randombytes(alices_ephemeral_key, POINT_SIZE);
 
-    init("alice.txt", alices_private_set, alices_ephemeral_key);
-    sort_prefs(alices_private_set, MAX_SET_SIZE);
+    // negotiate appropriate set size
+    unsigned int active_set_size = init("alice.txt", alices_private_set, alices_ephemeral_key);
+    unsigned int bobs_set_size;
+
+    write(network_ab[1], &active_set_size, sizeof(active_set_size));
+    read(network_ba[0], &bobs_set_size, sizeof(bobs_set_size));
+    if(active_set_size > bobs_set_size){
+        active_set_size = bobs_set_size;
+    }
+    sort_prefs(alices_private_set, active_set_size);
 
     int i;
-    for(i = 0; i < MAX_SET_SIZE; i++){
+    for(i = 0; i < active_set_size; i++){
         // send Alice's first point
         write(network_ab[1], alices_private_set[i].encrypted_pref, POINT_SIZE);
 
         // get it back encrypted
         read(network_ba[0], alices_private_set[i].encrypted_pref, POINT_SIZE);
-        sort_prefs(shared_private_set, MAX_SET_SIZE);
         // get Bob's first point
         read(network_ba[0], bobs_private_set[i].encrypted_pref, POINT_SIZE);
         bobs_private_set[i].offset = 0;
 
-        pref_t *result = bsearch(bobs_private_set+i, shared_private_set, MAX_SET_SIZE, sizeof(pref_t), compare_prefs);
+        crypto_scalarmult_curve25519(bobs_private_set[i].encrypted_pref, alices_ephemeral_key, bobs_private_set[i].encrypted_pref);
+
+        // cheating detection
+        sort_prefs(shared_private_set, active_set_size);
+        pref_t *result = bsearch(bobs_private_set+i, shared_private_set, active_set_size, sizeof(pref_t), compare_prefs);
         if(result){
             // duplicate ciphertext received after we sent something usable, ABORT
             printf("ALICE FEELS SOMEBODY IS CHEATING\n");
             exit(0);
         }
-        memcpy(shared_private_set[i].encrypted_pref, bobs_private_set[i].encrypted_pref, POINT_SIZE);
-
         // encrypt Bob's point
-        crypto_scalarmult_curve25519(bobs_private_set[i].encrypted_pref, alices_ephemeral_key, bobs_private_set[i].encrypted_pref);
+        memcpy(shared_private_set[i].encrypted_pref, bobs_private_set[i].encrypted_pref, POINT_SIZE);
+        // see if we are still interested
+        sort_prefs(shared_private_set, active_set_size);
+        if(compare(alices_private_set, shared_private_set, NULL, 0, active_set_size) < 1 && i > (active_set_size/2)){
+            printf("ALICE FEELS LIKE THIS IS GOING NOWHERE\n");
+            /*exit(0);*/
+        }
+
         // send it back
         write(network_ab[1], bobs_private_set[i].encrypted_pref, POINT_SIZE);
 
     }
 
-    sort_prefs(bobs_private_set, MAX_SET_SIZE);
-    compare(alices_private_set, bobs_private_set, "alice.txt");
+    sort_prefs(bobs_private_set, active_set_size);
+    compare(alices_private_set, bobs_private_set, "alice.txt", 1, active_set_size);
 }
 
 void* bob_thread(void* v){
+    /*unsigned int active_set_size = *((unsigned int*) v);*/
     pref_t bobs_private_set[MAX_SET_SIZE];
     pref_t alices_private_set[MAX_SET_SIZE];
     pref_t shared_private_set[MAX_SET_SIZE];
     memset(bobs_private_set, -1, sizeof(alices_private_set));
     unsigned char bobs_ephemeral_key[POINT_SIZE];
 
+    // initialize key
     randombytes(bobs_ephemeral_key,POINT_SIZE);
 
-    init("bob.txt", bobs_private_set, bobs_ephemeral_key);
-    sort_prefs(bobs_private_set, MAX_SET_SIZE);
+    // negotiate appropriate set size
+    unsigned int active_set_size = init("bob.txt", bobs_private_set, bobs_ephemeral_key);
+    unsigned int alices_set_size;
+
+    read(network_ab[0], &alices_set_size, sizeof(alices_set_size));
+    if(active_set_size > alices_set_size){
+        active_set_size = alices_set_size;
+    }
+    write(network_ba[1], &active_set_size, sizeof(active_set_size));
+    sort_prefs(bobs_private_set, active_set_size);
 
     int i;
-    for(i = 0; i < MAX_SET_SIZE; i++){
+    for(i = 0; i < active_set_size; i++){
         // receive Alice's first point
         read(network_ab[0], alices_private_set[i].encrypted_pref, POINT_SIZE);
 
-        sort_prefs(shared_private_set, MAX_SET_SIZE);
-        pref_t *result = bsearch(alices_private_set+i, shared_private_set, MAX_SET_SIZE, sizeof(pref_t), compare_prefs);
+        // encrypt it
+        crypto_scalarmult_curve25519(alices_private_set[i].encrypted_pref, bobs_ephemeral_key, alices_private_set[i].encrypted_pref);
+
+        // cheating detection
+        sort_prefs(shared_private_set, active_set_size);
+        pref_t *result = bsearch(alices_private_set+i, shared_private_set, active_set_size, sizeof(pref_t), compare_prefs);
         if(result){
             // duplicate ciphertext received after we sent something usable, ABORT
             printf("BOB FEELS SOMEBODY IS CHEATING\n");
@@ -139,8 +173,14 @@ void* bob_thread(void* v){
         }
         memcpy(shared_private_set[i].encrypted_pref, alices_private_set[i].encrypted_pref, POINT_SIZE);
 
-        // encrypt Alice's point and send it back
-        crypto_scalarmult_curve25519(alices_private_set[i].encrypted_pref, bobs_ephemeral_key, alices_private_set[i].encrypted_pref);
+        // see if we are still interested
+        sort_prefs(shared_private_set, active_set_size);
+        if(compare(bobs_private_set, shared_private_set, NULL, 0, active_set_size) < 1 && i > (active_set_size/2)){
+            printf("BOB FEELS LIKE THIS IS GOING NOWHERE\n");
+            /*exit(0);*/
+        }
+
+        // send encrypted point back
         write(network_ba[1], alices_private_set[i].encrypted_pref, POINT_SIZE);
         alices_private_set[i].offset = 0;
 
@@ -151,9 +191,9 @@ void* bob_thread(void* v){
         read(network_ab[0], bobs_private_set[i].encrypted_pref, POINT_SIZE);
     }
 
-    sort_prefs(alices_private_set, MAX_SET_SIZE);
-    sort_prefs(bobs_private_set, MAX_SET_SIZE);
-    compare(bobs_private_set, alices_private_set, "bob.txt");
+    sort_prefs(alices_private_set, active_set_size);
+    sort_prefs(bobs_private_set, active_set_size);
+    compare(bobs_private_set, alices_private_set, "bob.txt", 1, active_set_size);
 }
 
 void main(int argc, char* argv[]){
