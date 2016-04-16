a18485b91fd83c777934226ca7cb1296096141f9
Team Teledildonics <teledildonics@hmamail.com>
2016-04-16 23:55:15 +0200

    Ported PKI PoC to Python, and added kinks list from MojoUpgrade.com

--- python/pki.py
+++ python/pki.py
@@ -38,66 +38,66 @@ def compare(set_a, set_b, own):
     return conclusions
 
 import nacl.utils
-def alice(network_ab):
+def alice(network):
     # Alice sends her wants
     alices_prefs = map(lambda x: x.strip(), file("alice.txt").readlines())
     alices_ephemeral_key = nacl.utils.random(16)
     alices_encrypted_wants = init(alices_prefs, alices_ephemeral_key, False)
     # negotiate set size
     alices_prefs_count = len(alices_prefs)
-    network_ab.send(alices_prefs_count)
-    bobs_prefs_count = network_ab.recv()
+    network.send(alices_prefs_count)
+    bobs_prefs_count = network.recv()
     if alices_prefs_count > bobs_prefs_count:
         alices_prefs_count = bobs_prefs_count
     # compare prefs
     bobs_encrypted_haves = []
     for i in range(0, alices_prefs_count):
         # send Alice's point
-        network_ab.send(alices_encrypted_wants[i])
+        network.send(alices_encrypted_wants[i])
         # get it back encrypted
-        alices_encrypted_wants[i] = network_ab.recv()
+        alices_encrypted_wants[i] = network.recv()
         # get Bob's point and encrypt it
-        bobs_encrypted_have = c.crypto_scalarmult(alices_ephemeral_key, network_ab.recv())
+        bobs_encrypted_have = c.crypto_scalarmult(alices_ephemeral_key, network.recv())
         if bobs_encrypted_have in bobs_encrypted_haves:
             print "Alice feels somebody is cheating"
-            #return
+            return
         bobs_encrypted_haves.append(bobs_encrypted_have)
         # test if we are still interested
         if len(set.intersection(set(bobs_encrypted_haves), set(alices_encrypted_wants))) < i/10:
             print "Alice feels like this is going nowhere"
-        network_ab.send(bobs_encrypted_haves[-1])
+        network.send(bobs_encrypted_haves[-1])
     for i in compare(alices_encrypted_wants, bobs_encrypted_haves, alices_prefs):
         print "Alice learned:", i
 
-def bob(network_ba):
+def bob(network):
     # Bob sends his haves
     bobs_prefs = map(lambda x: x.strip(), file("bob.txt").readlines())
     bobs_ephemeral_key = nacl.utils.random(16)
     bobs_encrypted_haves = init(bobs_prefs, bobs_ephemeral_key, True)
     # negotiate set size
     bobs_prefs_count = len(bobs_prefs)
-    alices_prefs_count = network_ba.recv()
+    alices_prefs_count = network.recv()
     if bobs_prefs_count > alices_prefs_count:
         bobs_prefs_count = alices_prefs_count
-    network_ba.send(bobs_prefs_count)
+    network.send(bobs_prefs_count)
     # compare prefs
     alices_encrypted_wants = []
     for i in range(0, bobs_prefs_count):
         # receive Alice's point and encrypt it
-        alices_encrypted_want = c.crypto_scalarmult(bobs_ephemeral_key, network_ba.recv())
+        alices_encrypted_want = c.crypto_scalarmult(bobs_ephemeral_key, network.recv())
         if alices_encrypted_want in alices_encrypted_wants:
             print "Bob feels somebody is cheating"
-            #return
+            return
         alices_encrypted_wants.append(alices_encrypted_want)
         # test if we are still interested
         if len(set.intersection(set(bobs_encrypted_haves), set(alices_encrypted_wants))) < i/10:
             print "Bob feels like this is going nowhere"
         # send encrypted point back
-        network_ba.send(alices_encrypted_wants[-1])
+        network.send(alices_encrypted_wants[-1])
         # send bobs point
-        network_ba.send(bobs_encrypted_haves[i])
+        network.send(bobs_encrypted_haves[i])
         # get it back encrypted
-        bobs_encrypted_haves[i] = network_ba.recv()
+        bobs_encrypted_haves[i] = network.recv()
     for i in compare(bobs_encrypted_haves, alices_encrypted_wants, bobs_prefs):
         print "Bob learned:", i
 
